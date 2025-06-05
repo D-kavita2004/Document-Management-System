71b7e74a9db7ed6f3b1d57295d2cd3d30c342f46
dkavita <dkavita@smartcodersconsulting.com>
2025-06-05 16:15:37 +0530

    associated profiles and data

--- src/components/Pages/AdminSettings.tsx
+++ src/components/Pages/AdminSettings.tsx
@@ -13,149 +13,175 @@ import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Check } from "lucide-react";
 import { Input } from "../ui/input";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
 const AdminSettings = () => {
 
-const [attributes, setAttributes] = useState([]);
-
-const formAttributes = () => {
-  const data: Record<string, { Selected: boolean; Label: string }> = {};
-  documentKeys.forEach((key) => {
-    data[key] = { Selected: false, Label: "" };
-  });
-  setAttributes(data);
-  console.log(data);
+  type Attribute = {
+  attID: string;
+  Selected: boolean;
+  Label: string;
 };
+  const [attributes, setAttributes] = useState<Attribute[]>([]);
+  const [initialState, setInitialState] = useState<Attribute[]>([]);
 
-useEffect(()=>{
-  formAttributes();
-},[])
+  const [showDialog,setShowDialog] = useState(false);
+  const [changesSaved,setChangesSaved] = useState(false);
+  const [profile,setProfile] = useState("Admin");
+  const [changeInAttr,setChangeInAttr] = useState(false);
 
-const handleToggle = (key) => {
-  setAttributes(prev => ({
-    ...prev,
-    [key]: {
-      ...prev[key],
-      Selected: !prev[key].Selected,
+  const formAttributes = () => {
+    const listOfAttributes = documentKeys.map((key) => ({
+      attID: key,
+      Selected: false,
+      Label: "",
+    }));
+    console.log(listOfAttributes);
+    return listOfAttributes;
+  };
+
+useEffect(() => {
+  const initialAttrs = formAttributes();
+  setAttributes([...initialAttrs]);
+  setInitialState([...initialAttrs]);
+}, []);
+
+
+  const handleToggle = (id: string) => {
+    setAttributes((prev) =>
+      prev.map((item) =>
+        item.attID === id
+          ? { ...item, Selected: !item.Selected }
+          : item
+      )
+    );
+  };
+
+  const handleLabelChange = (id: string, value: string) => {
+    setAttributes((prev) =>
+      prev.map((item) =>
+        item.attID === id ? { ...item, Label: value } : item
+      )
+    );
+  };
+
+  const handleProfile = (value:string)=>{
+    if(changeInAttr && !changesSaved){
+      setShowDialog(true);
     }
-  }));
-};
+    else{
+      setProfile(value);
+      setAttributes([...initialState]);
+      setChangeInAttr(false);
+      setChangesSaved(false);
+    }
+  }
+
+  const handleOnSave = ()=>{
+      setChangesSaved(true);
+      alert("Changes saved successfully");
+      console.log("Pofile",profile);
+      console.log("attributes",attributes);
+      console.log("ok");
+  }
+useEffect(() => {
+  const isChanged = JSON.stringify(attributes) !== JSON.stringify(initialState);
+  setChangeInAttr(isChanged);
+}, [attributes, initialState]);
 
 
   return (
     <div className='flex pt-4 flex-col space-y-4 overflow-auto max-h-[85vh] scrollbar-hide'>
-        {/* Profile Selection */}
-        <div className='flex justify-center space-x-6 text-md'>
-          <Label className='text-md'>Select the Profile : </Label>
-          <Select>
-            <SelectTrigger className="w-[180px] cursor-pointer">
-              <SelectValue placeholder="Your Profile" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Profiles</SelectLabel>
-                <SelectItem value="apple">Admin</SelectItem>
-                <SelectItem value="banana">HR</SelectItem>
-                <SelectItem value="blueberry">User</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
+      {/* Profile Selection */}
+      <div className='flex justify-center space-x-6 text-md'>
+        <Label className='text-md'>Select the Profile : </Label>
+        <Select onValueChange={handleProfile} value={profile}>
+          <SelectTrigger className="w-[180px] cursor-pointer">
+            <SelectValue placeholder="Your Profile" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Profiles</SelectLabel>
+              <SelectItem value="Admin">Admin</SelectItem>
+              <SelectItem value="HR">HR</SelectItem>
+              <SelectItem value="User">User</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
 
-        {/* Attributes */}
-        <div className="max-h-[85%] pt-2 flex justify-evenly overflow-auto md:max-w-[70%] mx-auto max-w-[90%]">
-            <table className="border-collapse w-full h-full">
-              <thead className="bg-[#1A33A9] dark:bg-white dark:text-black text-white sticky top-0 z-10 ">
-                  <tr>
-                      <th className="px-2 py-2">All Attributes</th>
-                      <th className="px-2 py-2">Selected Attributes</th>
-                      <th className="px-2 py-2">New Label</th>
-                  </tr>
-              </thead>
-              <tbody>
-                {
-                  Object.keys(attributes).map((key)=>(
-                    <tr className="even:bg-gray-200 dark:bg-[#3b3636]">
-                        <td className="border border-gray-300 px-1 py-1 whitespace-nowrap text-center cursor-pointer" onClick={()=>handleToggle(key)}>{key}</td>
-                        <td className="border border-gray-300 px-1 py-1 text-center align-middle">
-                          <div className="flex items-center justify-center h-full">
-                            {attributes[key].Selected && <Check className="w-6 h-6" />}
-                          </div>
-                        </td>
+      {/* Attributes Table */}
+      <div className="max-h-[85%] pt-2 flex justify-evenly overflow-auto md:max-w-[70%] mx-auto max-w-[90%]">
+        <table className="border-collapse w-full h-full">
+          <thead className="bg-[#1A33A9] dark:bg-white dark:text-black text-white sticky top-0 z-10">
+            <tr>
+              <th className="px-2 py-2">Selected</th>
+              <th className="px-2 py-2">ID</th>
+              <th className="px-2 py-2">Label</th>
+            </tr>
+          </thead>
+          <tbody>
+            {attributes.map((item) => (
+              <tr key={item.attID} className="even:bg-gray-200 dark:bg-[#3b3636]">
+                <td className="border border-gray-300 px-1 py-1 text-center align-middle">
+                  <div className="flex items-center justify-center h-full">
+                    {item.Selected && <Check className="w-6 h-6" />}
+                  </div>
+                </td>
+                <td
+                  className="border border-gray-300 px-1 py-1 whitespace-nowrap text-center cursor-pointer"
+                  onClick={() => handleToggle(item.attID)}
+                >
+                  {item.attID}
+                </td>
+                <td className="border border-gray-300 px-2 py-1 min-w-[150px]">
+                  <Input
+                    placeholder="Enter..."
+                    value={item.Label}
+                    onChange={(e) => handleLabelChange(item.attID, e.target.value)}
+                    className="w-full text-center"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+          <Button
+          onClick={handleOnSave}
+          disabled={!changeInAttr} 
+          className="w-fit self-center mb-3 bg-[#1A33A9] dark:bg-white cursor-pointer">
+             Save Changes
+          </Button>      
 
-                        <td className="border border-gray-300 px-2 py-1 min-w-[150px]">
-                          <Input
-                            placeholder="Enter..."
-                            className="w-full text-center"
-                          />
-                        </td>
+      <AlertDialog open={showDialog} onOpenChange={setShowDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+               You have unsaved changes. Are you sure you want to switch the profile? Unsaved data will be lost.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={handleOnSave}>Save</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
 
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>          
-        </div>
 
-        <Button className="w-fit self-center mb-3 bg-[#1A33A9] dark:bg-white cursor-pointer">Save Changes</Button>
     </div>
   );
 };
 
 export default AdminSettings;
-
-        // {/* Available Attributes */}
-        // <div className="flex flex-col gap-3 max-w-[45%] md:w-auto">
-        //   <h3 className="font-bold">All available Attributes</h3>
-        //   <div className="h-full overflow-y-auto">
-        //     {documentKeys.map((key) => (
-        //       <div key={key} className="flex items-center gap-3 mb-3">
-        //         <Checkbox
-        //           id={key}
-        //           checked={selectedKeys.includes(key)}
-        //           onCheckedChange={() => handleToggle(key)}
-        //         />
-        //         <Label htmlFor={key} className="cursor-pointer">{key}</Label>
-        //       </div>
-        //     ))}
-        //   </div>
-        // </div>
-
-        // {/* Selected Attributes Table */}
-        // <div className="flex flex-col gap-3 max-w-[45%]">
-        //   <h3 className="font-bold">Selected Attributes</h3>
-        //   {selectedKeys.length === 0 ? (
-        //     <p className="text-sm italic text-gray-500">No Attributes selected</p>
-        //   ) : (
-        //     <div className="grid grid-cols-2 gap-3">
-        //       <Label className="font-semibold">Original Key</Label>
-        //       <Label className="font-semibold">New Label</Label>
-        //       {selectedKeys.map((key) => (
-        //         <div key={key} className="contents">
-        //           <Label className="self-center">{key}</Label>
-        //           <Input
-        //             value={customLabels[key] || key}
-        //             onChange={(e) => handleLabelChange(key, e.target.value)}
-        //             placeholder="Enter new label"
-        //             className="w-full"
-        //           />
-        //         </div>
-        //       ))}
-        //     </div>
-        //   )}
-        // </div>
-
-          // <table className="border-collapse w-full h-full">
-          //   <thead className="bg-[#1A33A9] dark:bg-white dark:text-black text-white sticky top-0 z-10">
-          //       <tr>
-          //           <th>All Attributes</th>
-          //           <th>Selected Attributes</th>
-          //           <th>New Label</th>
-          //       </tr>
-          //   </thead>
-          //   <tbody>
-          //     {
-          //       documentKeys.map((key)=>())
-          //     }
-          //   </tbody>
-          //   </table>
-         
